1e86a8e402eb56c559ac8efe48e4728c2174e411
suhaoudeh <soudeh@my.centennialcollege.ca>
2025-10-31 06:51:54 -0400

    Updated server.js and route files for contact and education authentication

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import authMiddleware from '../Middleware/auth.js'; 
+
 import {
   getAllContacts,
   getContactById,
@@ -10,11 +12,11 @@ import {
 
 const router = express.Router();
 
-router.get('/', getAllContacts);
-router.get('/:id', getContactById);
-router.post('/', createContact);
-router.put('/:id', updateContact);
-router.delete('/:id', deleteContact);
-router.delete('/', deleteAllContacts);
+router.get('/', authMiddleware, getAllContacts);
+router.get('/:id', authMiddleware, getContactById);
+router.post('/',authMiddleware, createContact);
+router.put('/:id',authMiddleware, updateContact);
+router.delete('/:id',authMiddleware, deleteContact);
+router.delete('/',authMiddleware, deleteAllContacts);
 
 export default router;
--- server/routes/qualificationRoutes.js
+++ server/routes/qualificationRoutes.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import authMiddleware from '../Middleware/auth.js';
+
 import {
   getAllQualifications,
   getQualificationById,
@@ -11,21 +13,21 @@ import {
 const router = express.Router();
 
 // GET all qualifications
-router.get('/', getAllQualifications);              // GET api/qualifications
+router.get('/', authMiddleware, getAllQualifications);              // GET api/qualifications
 
 // GET qualification by ID
-router.get('/:id', getQualificationById);           // GET api/qualifications/:id
+router.get('/:id', authMiddleware, getQualificationById);           // GET api/qualifications/:id
 
 // POST new qualification
-router.post('/', createQualification);              // POST api/qualifications
+router.post('/', authMiddleware, createQualification);              // POST api/qualifications
 
 // PUT update qualification by ID
-router.put('/:id', updateQualification);            // PUT api/qualifications/:id
+router.put('/:id', authMiddleware, updateQualification);            // PUT api/qualifications/:id
 
 // DELETE qualification by ID
-router.delete('/:id', deleteQualification);         // DELETE api/qualifications/:id
+router.delete('/:id', authMiddleware, deleteQualification);         // DELETE api/qualifications/:id
 
 // DELETE all qualifications
-router.delete('/', deleteAllQualifications);        // DELETE api/qualifications
+router.delete('/', authMiddleware, deleteAllQualifications);        // DELETE api/qualifications
 
 export default router;
